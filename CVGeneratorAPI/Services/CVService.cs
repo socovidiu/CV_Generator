5f8791bea64563b4a3883f867b1fc0e69c5d054d
Ovidiu Suciu <ovidiu.suciusoc@gmail.com>
2025-03-06 03:33:56 +0200

    Solve errors for backend app

--- CVGeneratorAPI/Services/CVService.cs
+++ CVGeneratorAPI/Services/CVService.cs
@@ -16,12 +16,12 @@ public class CVService
         _cvCollection = database.GetCollection<CVModel>(mongoDBSettings.Value.CollectionName);
     }
 
-    public async Task<List<CVModel>> GetAllAsync() 
+    public async Task<List<CVModel>> GetAllCvsAsync() 
     {
         return await _cvCollection.Find(_ => true).ToListAsync();
     } 
-    public async Task<CVModel?> GetByIdAsync(string id) => await _cvCollection.Find(cv => cv.Id == id).FirstOrDefaultAsync();
-    public async Task CreateAsync(CVModel cv) => await _cvCollection.InsertOneAsync(cv);
-    public async Task UpdateAsync(string id, CVModel cv) => await _cvCollection.ReplaceOneAsync(c => c.Id == id, cv);
-    public async Task DeleteAsync(string id) => await _cvCollection.DeleteOneAsync(c => c.Id == id);
+    public async Task<CVModel?> GetCvByIdAsync(string id) => await _cvCollection.Find(cv => cv.Id == id).FirstOrDefaultAsync();
+    public async Task CreateCvAsync(CVModel cv) => await _cvCollection.InsertOneAsync(cv);
+    public async Task UpdateCvAsync(string id, CVModel cv) => await _cvCollection.ReplaceOneAsync(c => c.Id == id, cv);
+    public async Task DeleteCvAsync(string id) => await _cvCollection.DeleteOneAsync(c => c.Id == id);
 }
